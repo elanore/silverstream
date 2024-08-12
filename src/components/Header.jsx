7d169fce794325e3414ef7ec3ea0a63bc1383b61
elanore <elanorejoseph@gmail.com>
2024-08-12 22:15:53 +0530

    fixing browse issue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import logo from "../images/logo1.png";
-import  signout  from "../images/signout.png";
-import { signOut } from "firebase/auth";
+import signout from "../images/signout.png";
+import { signOut, onAuthStateChanged } from "firebase/auth";
 import { auth } from "../utils/firebase";
-
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-
-
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { addUser, removeUser } from "../utils/userSlice";
 
 const Header = () => {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
-  const user = useSelector(store => store.user);
-  const handleSignOut=()=>{
+  const user = useSelector((store) => store.user);
+  const handleSignOut = () => {
     signOut(auth)
       .then(() => {
         navigate("/");
@@ -22,18 +22,36 @@ const Header = () => {
         // An error happened.
         navigate("/error");
       });
-  }
+  };
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const { uid, email, displayname, photoURL } = user;
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayname: displayname,
+            photoURL: photoURL,
+          })
+        );
+      } else {
+        // User is signed out
+        dispatch(removeUser());
+      }
+    });
+  }, []);
   return (
     <div className="w-screen absolute px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
-      <img
-        className="w-44"
-        src={logo}
-        alt="Logo"
-      />
-     {user && (
-      <div className="flex p-4">
+      <img className="w-44" src={logo} alt="Logo" />
+      {user && (
+        <div className="flex p-4">
           <img className="w-12 h-12 " src={user?.photoURL} alt="user-icon" />
-          <button onClick={handleSignOut}className="font-bold text-white">(Sign Out)</button>
+          <button onClick={handleSignOut} className="font-bold text-white">
+            (Sign Out)
+          </button>
         </div>
       )}
     </div>
